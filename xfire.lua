43f5d33b604b04e6d332bb8e15a783dcba797fc8
Arkiver2 <Arkiver@hotmail.com>
2015-06-20 02:10:52 +0200

    xfire.lua: exclude classic.xfire.com

--- xfire.lua
+++ xfire.lua
@@ -29,10 +29,10 @@ wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_pars
   end
   
   if (downloaded[url] ~= true or addedtolist[url] ~= true) then
-    if item_type == "video" and string.match(urlpos["url"]["host"], "xfire%.com") and string.match(url, "/"..item_value.."[a-z0-9]") and not string.match(url, "/"..item_value.."[a-z0-9][a-z0-9]") then
+    if item_type == "video" and string.match(urlpos["url"]["host"], "xfire%.com") and string.match(url, "/"..item_value.."[a-z0-9]") and not (string.match(url, "/"..item_value.."[a-z0-9][a-z0-9]") or string.match(url, "https?://classic%.xfire%.com")) then
       addedtolist[url] = true
       return true
-    elseif string.match(url, "https?://media%.xfire%.com/") and string.match(url, "%?bn=[0-9]+") and not string.match(url, "%?bn=46153") then
+    elseif string.match(url, "https?://media%.xfire%.com/") and string.match(url, "%?bn=[0-9]+") and not (string.match(url, "%?bn=46153") or string.match(url, "https?://classic%.xfire%.com")) then
       addedtolist[url] = true
       return true
     else
@@ -51,7 +51,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
   end
  
   local function check(url)
-    if (downloaded[url] ~= true and addedtolist[url] ~= true) and string.match(string.match(url, "https?://([^/]+)"), "xfire%.com") and string.match(url, "/"..item_value.."[a-z0-9]") and not string.match(url, "/"..item_value.."[a-z0-9][a-z0-9]") then
+    if (downloaded[url] ~= true and addedtolist[url] ~= true) and string.match(string.match(url, "https?://([^/]+)"), "xfire%.com") and string.match(url, "/"..item_value.."[a-z0-9]") and not (string.match(url, "/"..item_value.."[a-z0-9][a-z0-9]") or string.match(url, "https?://classic%.xfire%.com")) then
       if string.match(url, "&amp;") then
         table.insert(urls, { url=string.gsub(url, "&amp;", "&") })
         addedtolist[url] = true
